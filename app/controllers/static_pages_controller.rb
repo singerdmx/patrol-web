8ee48680c367b1d98835be3fe723bf2842251adf
Yin Ding <dingyin@gmail.com>
2014-04-25 10:01:05 -0700

    update user sign out link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def home
-    redirect_to new_user_session_path unless user_signed_in?
-
-    redirect_to "/users/#{current_user.id}"
+    if user_signed_in?
+      redirect_to "/users/#{current_user.id}"
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def help
