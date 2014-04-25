136010fd718c3248d73d68df282f6c79a806f262
Xin Yao <singerdmx@gmail.com>
2014-04-25 09:34:25 -0700

    Static page redirect to user page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     redirect_to new_user_session_path unless user_signed_in?
 
-    #redirect_to "/users/#{current_user.email}"
+    redirect_to "/users/#{current_user.id}"
   end
 
   def help
