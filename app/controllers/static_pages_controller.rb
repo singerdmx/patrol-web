99328ebdccd8e0e6c532d5f47685daef65c63eff
Yin Ding <dingyin@gmail.com>
2014-04-26 00:11:44 -0700

    stay in homepage when login fails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,6 @@ class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
       redirect_to "/users/#{current_user.id}"
-    else
-      redirect_to new_user_session_path
     end
   end
 
