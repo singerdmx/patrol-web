d99e6df9fa6f814830c60ba7654a146167c96ee3
Xin Yao <singerdmx@gmail.com>
2014-05-16 12:33:57 -0700

    Add placeholder for barcode input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -216,4 +216,25 @@ function getDatetimePickerSettings() {
         pick12HourFormat: true,
         endDate: getToday()
     }
-};
+}
+
+// Support for HTML5 Placeholder Text in IE
+$(function() {
+    if(!$.support.placeholder) {
+        var active = document.activeElement;
+        $(':text').focus(function () {
+            if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+                $(this).val('').removeClass('hasPlaceholder');
+            }
+        }).blur(function () {
+                if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+                    $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+                }
+            });
+        $(':text').blur();
+        $(active).focus();
+        $('form').submit(function () {
+            $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
+        });
+    }
+});
