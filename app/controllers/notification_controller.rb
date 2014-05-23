f3680a712ecf402d2f250163ed209a9bc06e6901
Xin Yao <singerdmx@gmail.com>
2014-05-23 09:24:29 -0700

    Bug fix for routes.rb (Please DO NOT change user route, it's for authentication)

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,17 +2,14 @@ class NotificationController < ApplicationController
   def index
     @notifications_json = []
     now = Time.now
-    current_user.all_points.each { |point|
+    current_user.all_points.each do |point|
       last_check_time = point.check_results.maximum(:check_time)
       logger.info("last_check_time : #{last_check_time}")
       if !last_check_time.nil? &&  last_check_time + point.frequency * 3600 < now
-
-        @notifications_json<<  "点 #{point.id} #{point.name} 检修超期"
+        @notifications_json << "#{point.name} #{point.description} 检修超期"
       end
+    end
 
-    }
     render template: 'notification/index', status: :ok
-
-
   end
 end
