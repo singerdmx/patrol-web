52a8ec121d50af485bcaae65584838d1a223fb06
feeb <feefee@gmail.com>
2014-05-23 09:39:44 -0700

    adding etag support and minor wording adjustment

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -2,14 +2,16 @@ class NotificationController < ApplicationController
   def index
     @notifications_json = []
     now = Time.now
-    current_user.all_points.each do |point|
+    current_user.all_points.each { |point|
       last_check_time = point.check_results.maximum(:check_time)
-      logger.info("last_check_time : #{last_check_time}")
       if !last_check_time.nil? &&  last_check_time + point.frequency * 3600 < now
-        @notifications_json << "#{point.name} #{point.description} 检修超期"
+          @notifications_json<<  "#{point.name}  #{point.barcode}  巡检到期"
       end
+    }
+    if stale?(etag: @notifications_json)
+      render template: 'notification/index', status: :ok
+    else
+      head :not_modified
     end
-
-    render template: 'notification/index', status: :ok
   end
 end
