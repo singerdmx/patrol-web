51236f29ed0c4f603cb3cfedd1e3be1691924e4d
feeb <feefee@gmail.com>
2014-05-28 10:16:58 -0700

    fix notification

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -3,18 +3,16 @@ class NotificationController < ApplicationController
     @notifications_json = []
     now = Time.now
     last_modified = nil
-    previous_modified = request.headers["Last-Modified"]
+    previous_modified = request.headers["If-Modified-Since"]
     previous_modified = Time.parse(previous_modified) if  !previous_modified.nil?
     current_user.all_points.each { |point|
       last_check_time = point.check_results.maximum(:check_time)
-      if last_check_time.nil?
-          #TODO : do we need to care those never checked?
-          @notifications_json<<  "#{point.name}  #{point.barcode}  从未检查过"
-      elsif last_check_time + point.frequency * 3600 < now && (previous_modified.nil? || last_check_time> previous_modified)
+
+      if !last_check_time.nil? && last_check_time + point.frequency * 3600 < now && (previous_modified.nil? || last_check_time> previous_modified)
           @notifications_json<<  "#{point.name}  #{point.barcode}  巡检到期, 上次检查时间 : #{last_check_time.localtime}"
           if  last_modified.nil?
             last_modified = last_check_time
-          elsif
+          else
             last_modified = [last_modified, last_check_time].max
           end
 
