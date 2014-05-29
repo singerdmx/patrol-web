b50571d6427d607470b535c99b8ec8fbad302209
Yin Ding <dingyin@gmail.com>
2014-05-28 22:19:26 -0700

    require leader role to view factory tree

--- app/controllers/factories_controller.rb
+++ app/controllers/factories_controller.rb
@@ -4,41 +4,45 @@ class FactoriesController < ApplicationController
 
   # GET /factoriess.json
   def index
-    factories = Factory.all
+    if current_user.is_leader?
+      factories = Factory.all
 
-    @factories_json = []
-    factories.reverse.each do |f|
-      entry = to_hash(f)
-      entry['subfactories'] = []
+      @factories_json = []
+      factories.reverse.each do |f|
+        entry = to_hash(f)
+        entry['subfactories'] = []
 
-      f.subfactories.reverse.each do |subf|
-        subfentry = to_hash(subf)
-        subfentry['areas'] = []
+        f.subfactories.reverse.each do |subf|
+          subfentry = to_hash(subf)
+          subfentry['areas'] = []
 
-        subf.areas.reverse.each do |area|
-          areaentry = to_hash(area)
-          areaentry['routes'] = []
-          area.check_routes.reverse.each do |r|
-            areaentry['routes'] << to_hash(r)
+          subf.areas.reverse.each do |area|
+            areaentry = to_hash(area)
+            areaentry['routes'] = []
+            area.check_routes.reverse.each do |r|
+              areaentry['routes'] << to_hash(r)
+            end
+            subfentry['areas'] << areaentry
           end
-          subfentry['areas'] << areaentry
+
+          entry['subfactories'] << subfentry
         end
 
-        entry['subfactories'] << subfentry
+        @factories_json << entry
       end
 
-      @factories_json << entry
-    end
+      if params[:ui] == 'true'
+        @factories_json = index_ui_json_builder(@factories_json)
+      end
 
-    if params[:ui] == 'true'
-      @factories_json = index_ui_json_builder(@factories_json)
-    end
-
-    if stale?(etag: @factories_json.to_a,
-              last_modified: factories.maximum(:updated_at))
-      render template: 'factories/index', status: :ok
+      if stale?(etag: @factories_json.to_a,
+                last_modified: factories.maximum(:updated_at))
+        render template: 'factories/index', status: :ok
+      else
+        head :not_modified
+      end
     else
-      head :not_modified
+      render json: {error: "此用户没有足够权限访问本页"}.to_json, status: :unauthorized
     end
   end
 end
