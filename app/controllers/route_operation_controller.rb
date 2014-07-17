84e11a7b67574ab85f0cfaee05a2c4dd115c6665
Xin Yao <xyao@vmware.com>
2014-07-17 14:58:14 -0700

    Update RouteOperationController update method

--- app/controllers/route_operation_controller.rb
+++ app/controllers/route_operation_controller.rb
@@ -1,5 +1,13 @@
 class RouteOperationController < ApplicationController
-  def update
 
+  def update
+    unless current_user.is_admin?
+      render json: {:message => '您没有权限进行本次操作！'}.to_json, status: :unauthorized
+      return
+    end
+
+    params[:check_route_id]
+    params[:point]
+    render json: { success: true }.to_json, status: :ok
   end
 end
