7c934099a3abd7e125160c3503cb01663dd1dfa4
Xin Yao <xyao@vmware.com>
2014-10-09 14:32:20 -0700

    Render graph using awesome js

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -2,21 +2,21 @@ class GraphsController < ApplicationController
 
   # sample url: http://localhost:3000/graphs?check_point_id=3&type=pareto&check_time=1404111600..1412838000
   def show
-    point = CheckPoint.find(params[:check_point_id])
+    @point = CheckPoint.find(params[:check_point_id])
 
-    puts params[:type]
+    @type = params[:type]
     unless params[:check_time].nil?
       update_check_time(params)
     end
-    puts params[:check_time]
 
-    case point.category
+    fail "Invalid type #{@type}" unless ['default', 'horizontal bars', 'pareto', 'pie', 'exploded pie', 'doughnut'].include?(@type)
+
+    case @point.category
       when 40, 41
         @results = CheckResult.where(check_result_params).group(:result).count
       else
         fail "Category #{point.category} is supported!"
     end
-
   rescue Exception => e
     flash[:error] = e.message
   end
