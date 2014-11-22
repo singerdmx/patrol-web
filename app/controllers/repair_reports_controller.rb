35ecc6c1a9be604143c064ffa94c7d1dc4813e40
Xin Yao <xyao@vmware.com>
2014-11-22 00:30:07 -0800

    Implement POST /repair_reports.json controller

--- app/controllers/repair_reports_controller.rb
+++ app/controllers/repair_reports_controller.rb
@@ -1,2 +1,22 @@
 class RepairReportsController < ApplicationController
+
+  # GET /repair_reports
+  def index
+
+  rescue Exception => e
+    flash[:error] = e.message
+  end
+
+  # POST /repair_reports.json
+  def create
+    #params['created_by'] = current_user.id
+    report = params.select do |key, value|
+      key.in?(RepairReport.column_names())
+    end.symbolize_keys
+
+    RepairReport.create!(report)
+    render nothing: true, status: :created
+  rescue Exception => e
+    render json: {message: e.to_s}.to_json, status: :internal_server_error
+  end
 end
