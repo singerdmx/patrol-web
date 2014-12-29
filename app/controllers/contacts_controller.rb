c0b5c44fcb88e9c2c6dabdc54227004f79610b02
Yin Ding <yding@vmware.com>
2014-12-29 10:43:17 -0800

    temp: test send email in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -28,6 +28,8 @@ class ContactsController < ApplicationController
       return
     end
 
+    AlertMailer.alert_email(current_user).deliver
+
     Contact.create! do |c|
       c.name = params[:name]
       c.email = params[:email]
