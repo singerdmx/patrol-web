841c02356d175ea13fa393e8a656a2eca87644fd
Yin Ding <yding@vmware.com>
2014-12-29 10:42:32 -0800

    Add smtp setting for development ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,16 @@ Blog::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #mailer
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password'],
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
 end
