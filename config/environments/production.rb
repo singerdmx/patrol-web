c55e2a1f52e4e646e96c328b29b4ce4439ed60c9
Yin Ding <yding@vmware.com>
2014-12-29 23:19:52 -0800

    Try hardcode smtp in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Blog::Application.configure do
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => 'notification.mbrite@gmail.com',
+    :password             => 'tempest@123',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
