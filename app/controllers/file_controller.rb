d59495b61387da13984d755b78474fc273977c02
Xin Yao <xyao@vmware.com>
2015-01-22 16:29:28 -0800

    Temporary checkin for file upload

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,6 +1,15 @@
 class FileController < ApplicationController
   def create
-    file = params[:files]
-    render json: { success: true, file: file.first.original_filename }.to_json, status: :ok
+    # file = params[:files] for csv upload
+    file = params[:file].read
+
+    file_name = "/Users/xyao/Downloads/" + params[:file].original_filename
+    puts file_name
+    File.open(file_name, 'wb') do |f|
+      f.write(file)
+    end
+    render json: { success: true, id: 11, file: params[:file].original_filename }.to_json, status: :created
+  rescue Exception => e
+    render json: {message: e.to_s}.to_json, status: :internal_server_error
   end
 end
