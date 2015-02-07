bfc6363940137371982ef9544b8bb23128feb6e5
Xin Yao <xyao@vmware.com>
2015-02-07 08:47:41 -0800

    Add upload img to aws in file_controller

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -1,18 +1,23 @@
 class FileController < ApplicationController
   def create
     # file = params[:files] for csv upload
-    file = params[:file].read
+    upload_img(params)
 
-    file_name = "/Users/xyao/Downloads/" + params[:file].original_filename
-    puts file_name
-    File.open(file_name, 'wb') do |f|
-      f.write(file)
-    end
-
-    s3 = Aws::S3::Client.new(credentials: Settings::AWS_CREDENTIALS, region: Settings::AWS_REGION)
-    x = s3.list_objects(bucket: Settings::AWS_IMG_BUCKET)
-    render json: { success: true, id: 11, file: params[:file].original_filename }.to_json, status: :created
+    render json: { success: true, id: 11 }.to_json, status: :created
   rescue Exception => e
     render json: {message: e.to_s}.to_json, status: :internal_server_error
+  end
+
+  private
+
+  def upload_img(params)
+    file = params[:file].read
+
+    file_name = params[:file].original_filename
+    # refer to http://docs.aws.amazon.com/sdkforruby/api/Aws/S3/Client.html#put_object-instance_method
+    s3 = Aws::S3::Client.new(credentials: Settings::AWS_CREDENTIALS, region: Settings::AWS_REGION)
+    response = s3.put_object(bucket: Settings::AWS_IMG_BUCKET, key: file_name, body: file, acl: 'public-read')
+    url = response.instance_variable_get('@http_request').instance_variable_get('@endpoint').to_s
+    puts url
   end
 end
