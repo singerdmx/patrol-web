1e4ff3ae12b39156816bd9e6599a9def08e09fde
Yin Ding <yding@vmware.com>
2015-02-11 23:55:36 -0800

    Change mailer to use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,13 +80,15 @@ Blog::Application.configure do
 
   #mailer
   config.action_mailer.delivery_method = :smtp
-  # SMTP settings for gmail
+  config.action_mailer.default_url_options = {
+    :host => 'managebrite.com' }
   ActionMailer::Base.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :user_name            => 'notification.mbrite@gmail.com',
-    :password             => 'tempest@123',
-    :authentication       => "plain",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
