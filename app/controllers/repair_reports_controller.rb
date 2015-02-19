63b8495da1020a15604c39b8a26cd88316d9de5b
Xin Yao <xyao@vmware.com>
2015-02-18 19:54:15 -0800

    Update POST /repair_reports.json to accept multiple reports in one call

--- app/controllers/repair_reports_controller.rb
+++ app/controllers/repair_reports_controller.rb
@@ -13,12 +13,19 @@ class RepairReportsController < ApplicationController
 
   # POST /repair_reports.json
   def create
-    params['created_by_id'] = current_user.id
-    report = params.select do |key, value|
-      key.in?(RepairReport.column_names())
-    end.symbolize_keys
+    fail "no data when trying to create repair report!" if params[:repairReports].blank? && params[:ticketUpdates].blank?
 
-    RepairReport.create!(report)
+    start_time_ = Time.at(params[:start_time]).to_datetime
+    end_time_ = Time.at(params[:end_time]).to_datetime
+
+    params[:repairReports].each do |repair_report|
+      repair_report['created_by_id'] = current_user.id
+      report = repair_report.select do |key, value|
+        key.in?(RepairReport.column_names())
+      end.symbolize_keys
+      RepairReport.create!(report)
+    end
+
     render nothing: true, status: :created
   rescue Exception => e
     render json: {message: e.to_s}.to_json, status: :internal_server_error
