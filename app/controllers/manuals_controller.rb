b3b2f468343b5108845cb504e1663377302fc21e
Xin Yao <xyao@vmware.com>
2015-02-19 11:57:39 -0800

    Fix manuals GET 304 not working issue

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -3,13 +3,16 @@ class ManualsController < ApplicationController
   # GET /manuals.json
   def index
     manuals = {}
-    Manual.all.each do |m|
+    all_manuals = Manual.all
+    all_manuals.all.each do |m|
       manuals[m[:id]] = m[:entry]
     end
-    if stale?(etag: manuals)
+    if stale?(etag: manuals, last_modified: all_manuals.maximum(:updated_at))
       render json: manuals.to_json
     else
       head :not_modified
     end
+  rescue Exception => e
+    render json: {message: e.to_s}.to_json, status: :internal_server_error
   end
 end
