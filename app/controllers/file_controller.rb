7b20f3dbcde28cd9ad2e5986e99a80cb1d7a017f
Xin Yao <xyao@vmware.com>
2015-03-06 15:00:44 -0800

    Upload img to local

--- app/controllers/file_controller.rb
+++ app/controllers/file_controller.rb
@@ -14,6 +14,20 @@ class FileController < ApplicationController
     file = params[:file].read
 
     file_name = params[:file].original_filename
+
+    if Settings::STORE_IMAGE_LOCAL
+      img_dir = File.join(File.dirname(__FILE__), '../assets/images/img')
+      Dir.mkdir(img_dir) unless File.exists?(img_dir)
+
+      File.open(File.join(img_dir, file_name), 'wb') do |f|
+        f.write(file)
+      end
+
+      url = request.original_url[0..request.original_url.rindex('/')] + 'assets/img/' + file_name
+      image = ResultImage.create!(name: file_name,url: url)
+      return image.id
+    end
+
     # refer to http://docs.aws.amazon.com/sdkforruby/api/Aws/S3/Client.html#put_object-instance_method
     s3 = Aws::S3::Client.new(credentials: Settings::AWS_CREDENTIALS, region: Settings::AWS_REGION)
     response = s3.put_object(bucket: Settings::AWS_IMG_BUCKET, key: file_name, body: file, acl: 'public-read')
