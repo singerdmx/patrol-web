e9738477119db84a63417b0d89d1b87dfc2d24f9
Xin Yao <xyao@vmware.com>
2015-04-02 07:13:52 -0700

    Limit max lines of manual entry when showing in UI table

--- app/helpers/manuals_helper.rb
+++ app/helpers/manuals_helper.rb
@@ -15,9 +15,36 @@ module ManualsHelper
       [
         entry['id'],
         entry['name'],
-        entry['entry'],
+        get_manual_ui_entry(assets.length, entry['entry']),
         assets
       ]
     end
+  end
+
+  def get_manual_ui_entry(asset_num, entry)
+    content = ''
+    line = 0
+    count = 0
+    max_line_length = 30
+    max_lines = [3, asset_num].max
+    entry.length.times.each do |i|
+      break if line >= max_lines
+      c = entry[i]
+      if c.match(/^\n$/)
+        content += '<br/>'
+        count = 0
+        line += 1
+      else
+        content += c
+        count += 1
+      end
+
+      if count == max_line_length
+        content += '<br/>'
+        count = 0
+        line += 1
+      end
+    end
+    content
   end
 end
