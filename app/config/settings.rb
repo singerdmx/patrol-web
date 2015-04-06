7b42ab3c80246eaac989bbae61e04e838672fdfe
Yin Ding <yding@vmware.com>
2015-04-06 06:13:33 -0700

    Make media files stored in aws as default.

--- app/config/settings.rb
+++ app/config/settings.rb
@@ -1,7 +1,7 @@
 module RbConfig
   class Settings
     CATEGORY_SCAN_ONLY = Set.new [10]
-    STORE_MEDIA_LOCAL = true
+    STORE_MEDIA_LOCAL = false
     AWS_CREDENTIALS = Aws::Credentials.new('AKIAIYTZ7BX2OL3J7MJQ', 'ud6FxfIjAUyIGg+TYbYx+tu5OSapR706mlZxcizj')
     AWS_REGION = 'us-west-2'
   end
